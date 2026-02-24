b1fd3f4e9eb35e9e7a6d9fe709c6ce16d3618a55
mj <qjsalswn@gmail.com>
2026-02-24 10:04:30 +0900

    [Init] 프로젝트 생성 및 초기 설정

---
+++ src/main/java/org/example/practicebackend/PracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.practicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practicebackend/PracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
